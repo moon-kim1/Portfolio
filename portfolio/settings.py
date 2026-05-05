739f86da51add99f22666241f788b74c2ea04856
Moon Kim <moon_kim1@baylor.edu>
2026-05-05 15:30:24 -0500

    update ALLOWED_HOSTS with render domain

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-$8kg4!ydo#8e$eo87etu4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'portfolio-02n3.onrender.com,localhost,127.0.0.1').split(',')
 
 
 # Application definition
