fb6e161b0d330fecc2d49028eed4d9b5e90f5928
Moon Kim <moon_kim1@baylor.edu>
2026-05-05 20:29:44 -0500

    fixed issue

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-$8kg4!ydo#8e$eo87etu41ise$gu6-xxg$ho9if+2l*-r%^612')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = True
+# DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'portfolio-moon.onrender.com,portfolio-02n3.onrender.com,localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'portfolio-moon.onrender.com,portfolio-02n3.onrender.com,localhost,127.0.0.1').split(',')
 
 
 # Application definition
